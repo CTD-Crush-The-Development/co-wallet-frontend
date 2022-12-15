c14c6d4fddf05df025bb494d748061a7c2aebefb
jhoon6886@gmail.com <pwhyun6246>
2022-12-15 23:06:38 +0900

    first init

---
+++ src/App.ts
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        header
+      </div>
+      <div>
+        container
+        <ul>
+
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+
+javascript js. => .js.  x
+typesc ts => .tsx
---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='header'>
+        header
+      </div>
+      <div className='container'>
+        container
+        <ul>
+          <li>
+            1
+          </li>
+          <li>
+            2
+          </li>
+          <li>
+            3
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
