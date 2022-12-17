5782bd7121016d763b1eb3de3aa278ddfcac5554
이정환 <hwanlee@nts-corp.com>
2022-12-18 01:46:27 +0900

    init: storybook 설정

---
+++ src/pages/Login/Login.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Login from './index';
+
+export default {
+  component: Login,
+  title: 'pages/Login',
+};
+
+const Template = () => <Login />;
+
+export const Default = Template.bind({});
---
+++ src/pages/Main/Main.stories.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Main from './index';
+
+export default {
+  component: Main,
+  title: 'pages/Main',
+};
+
+const Template = () => <Main />;
+
+export const Default = Template.bind({});
